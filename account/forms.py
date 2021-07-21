9476db978b40aa7df0aa0399355ac7990267588c
Earthman12 <dteran@lpl.arizona.edu>
2021-07-21 09:52:26 -0700

    Little changes to the errors that get displayed when trying to login

--- account/forms.py
+++ account/forms.py
@@ -30,8 +30,10 @@ class AccountAuthenticationForm(forms.ModelForm):
 
     #   It is available to any form that extends the model form. Acts as an interceptor, before the form can do anything it has to run the clean() method and then any logic that is in the clean() method will get executed before the form can do anything and is useful to make sure a users credentials are valid and if they are not then we can provide feedback
     def clean(self):
-        email = self.cleaned_data['email']
-        password = self.cleaned_data['password']
+        #   Need to check if the form is valid or essentially check that a proper email is inputted
+        if self.is_valid():
+            email = self.cleaned_data['email']
+            password = self.cleaned_data['password']
 
-        if not authenticate(email = email, password = password):
-            raise forms.ValidationError("Invalid Login Credentials")
+            if not authenticate(email = email, password = password):
+                raise forms.ValidationError("Invalid Login Credentials")
