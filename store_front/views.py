b4c6e62aa70a24f5079a885855ff76a46b774e8b
Earthman12 <dteran@lpl.arizona.edu>
2021-08-11 15:46:55 -0700

    Added blog posts to the home screen so they all display

--- store_front/views.py
+++ store_front/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from operator import attrgetter
 from store_front.models import Product
+from blog.models import BlogPost
 
 # Create your views here.
 
@@ -10,5 +12,9 @@ def store_home_view(request):
     #   A 'select all query' from the database and passing it as context
     products = Product.objects.all()
     context['products'] = products
+
+    #   Query all the blog posts but we want to sort them. This line is going to query all the blog_posts and then sort the list based on the key 'date_updated' and then it will reverse it so the newest post will be at the top
+    blog_posts = sorted(BlogPost.objects.all(), key = attrgetter('date_updated'), reverse = True)
+    context['blog_posts'] = blog_posts
 
     return render(request, 'store_front/store_home.html', context)
