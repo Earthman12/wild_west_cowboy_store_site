ecf1253060e173a5f4b117be4c1ed90c55901701
Earthman12 <dteran08@gmail.com>
2021-09-01 11:47:49 -0700

    Deleted the existing products and added images to the product model

--- store_front/models.py
+++ store_front/models.py
@@ -2,12 +2,22 @@ from django.db import models
 
 
 # Create your models here.
+
+#   Function for defining the upload location for the images that will be associated with the product. This is taken from blog model
+def upload_location(instance, filename, **kwargs):
+    #   'product_id' is the id of the product, 'product_name' is the name of the product, 'filename' is the name of file that comes from the computer of the one who uploads it. .format() converts it all to a string
+    file_path = 'store_home/{product_id}/{product_name}-{filename}'.format(
+        product_id = str(instance.id), product_name = str(instance.name), filename = filename
+    )
+    return file_path
+
 class Product(models.Model):
 
     name = models.CharField(verbose_name="name", max_length=50)
     stock = models.IntegerField(verbose_name="stock", default=0)
     description = models.CharField(verbose_name="description", max_length=100)
     price = models.FloatField(verbose_name="price", default=0.0)
+    image = models.ImageField(upload_to = upload_location, null = False, blank = False)#This cant be used unless Pillow library is used
 
     def __str__(self):
         return self.name
@@ -15,4 +25,4 @@ class Product(models.Model):
     #   This changes the display names in admin page
     class Meta:
         verbose_name = "Product"
-        verbose_name_plural = "All Products"
+        verbose_name_plural = "Products"
